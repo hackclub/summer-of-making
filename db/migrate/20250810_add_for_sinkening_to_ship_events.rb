ab011fdfd5cad0bf71b154a8edf3df356724acf3
Kartikey Chauhan <me@cskartikey.dev>
2025-08-28 02:12:10 +0530

    try to fix new migrations

--- db/migrate/20250810_add_for_sinkening_to_ship_events.rb
+++ db/migrate/20250810_add_for_sinkening_to_ship_events.rb
@@ -1,5 +1,8 @@
 class AddForSinkeningToShipEvents < ActiveRecord::Migration[6.0]
-  def change
+  def up
     add_column :ship_events, :for_sinkening, :boolean, default: false, null: false
+  end
+  def down
+    # intentionally left blank
   end
 end
