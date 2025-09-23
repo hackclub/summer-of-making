deb92a7290692b3885f620e6929a9e666dfcaaf6
24c02 <163450896+24c02@users.noreply.github.com>
2025-09-23 17:27:22 -0400

    add backfill for magic happening reporters

---
+++ app/jobs/one_time/backfill_magic_reporters_job.rb
@@ -0,0 +1,19 @@
+class OneTime::BackfillMagicReportersJob < ApplicationJob
+  queue_as :default
+
+  def perform(args)
+    projects = Project
+                 .magical # magicked_at is not nil
+                 .where(magic_reporter: nil) # no reporter already
+                 .joins(:versions) # bring in versions
+                 .where("(versions.object_changes::jsonb) ? 'magicked_at'") # but only versions changing magicked_at
+                 .select("projects.*, versions.whodunnit as magic_reporter_whodunnit") # and pluck the responsible user
+
+    projects.each do |project|
+      next unless project.magic_reporter_whodunnit
+
+      project.update!(magic_reporter_id: project.magic_reporter_whodunnit&.to_i)
+    end
+  end
+
+end
