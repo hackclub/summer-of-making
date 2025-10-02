c367228584aecf3bafd05fe1f4ffba96ba9d0d81
Eric Zilvytis <eric@nezser.com>
2025-10-02 15:28:55 +0000

    auto block after deadline

--- app/controllers/projects/recertifications_controller.rb
+++ app/controllers/projects/recertifications_controller.rb
@@ -4,18 +4,25 @@ class Projects::RecertificationsController < ApplicationController
   def create
     authorize @project, :request_recertification?
 
-
-    deadline = Time.zone.local(2025, 10, 2, 23, 59, 59)
+    deadline = local(2025, 10, 2, 23, 59, 59)
     if Time.current > deadline
-      current_user.add_permission("no_recert")
-      redirect_to project_path(@project), alert: "Summer of Making is over! You used your one post-deadline recert request."
-      return
+      if current_user.has_permission?("post_deadline_recert_used")
+        current_user.add_permission("no_recert")
+        redirect_to project_path(@project), alert: "You already used your one post-deadline recert chance."
+        return
+      else
+        current_user.add_permission("post_deadline_recert_used")
+      end
     end
 
     instructions = params[:recertification_instructions]
 
     if @project.request_recertification!(instructions)
-      redirect_to project_path(@project), notice: "Re-certification requested! Your project will be reviewed again."
+      if Time.current > deadline
+        redirect_to project_path(@project), notice: "Last Re-certification requested! Your project will be reviewed again."
+      else
+        redirect_to project_path(@project), notice: "Re-certification requested! Your project will be reviewed again."
+      end
     else
       redirect_to project_path(@project), alert: "Cannot request re-certification for this project."
     end
