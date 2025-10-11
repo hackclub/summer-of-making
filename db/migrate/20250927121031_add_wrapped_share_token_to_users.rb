cd4e1ae800205c466c095e413fd2cf39dd1b9ef9
Mahad Kalam <mahadkalam1@proton.me>
2025-10-11 20:13:27 +0100

    Prevent locking migration

--- db/migrate/20250927121031_add_wrapped_share_token_to_users.rb
+++ db/migrate/20250927121031_add_wrapped_share_token_to_users.rb
@@ -1,6 +1,8 @@
 class AddWrappedShareTokenToUsers < ActiveRecord::Migration[8.0]
+  disable_ddl_transaction!
+
   def change
     add_column :users, :wrapped_share_token, :string
-    add_index :users, :wrapped_share_token, unique: true
+    add_index :users, :wrapped_share_token, unique: true, algorithm: :concurrently
   end
 end
