e331f3fb473bbb0eca1c2a4263d41ab4a1ece75a
Mahad Kalam <mahadkalam1@proton.me>
2025-10-11 20:42:05 +0100

    They keep locking..

--- db/migrate/20250927121031_add_wrapped_share_token_to_users.rb
+++ db/migrate/20250927121031_add_wrapped_share_token_to_users.rb
@@ -1,6 +1,12 @@
-class AddWrappedShareTokenToUsers < ActiveRecord::Migration[8.0]
-  def change
+class AddWrappedShareTokenColumnToUsers < ActiveRecord::Migration[8.0]
+  def up
+    old_timeout = select_value("SHOW lock_timeout")
+    execute "SET lock_timeout = '20s';"
     add_column :users, :wrapped_share_token, :string
-    add_index :users, :wrapped_share_token, unique: true
+    execute "SET lock_timeout = '#{old_timeout}';"
+  end
+
+  def down
+    remove_column :users, :wrapped_share_token
   end
 end
---
+++ db/migrate/20251011193937_add_wrapped_share_token_index_to_users.rb
@@ -0,0 +1,5 @@
+class AddWrappedShareTokenIndexToUsers < ActiveRecord::Migration[8.0]
+  def change
+    add_index :users, :wrapped_share_token, unique: true, algorithm: :concurrently
+  end
+end
