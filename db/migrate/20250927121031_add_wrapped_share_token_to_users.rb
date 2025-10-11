ae228d96d75e30259fe3daee7a42d124d835269c
Mahad Kalam <55807755+SkyfallWasTaken@users.noreply.github.com>
2025-10-11 20:49:22 +0100

    they see me lockin', they hatin'.. (#1225)
    * They keep locking..
    
    * Uncook pt2

--- db/migrate/20250927121031_add_wrapped_share_token_to_users.rb
+++ db/migrate/20250927121031_add_wrapped_share_token_to_users.rb
@@ -1,8 +1,12 @@
 class AddWrappedShareTokenToUsers < ActiveRecord::Migration[8.0]
-  disable_ddl_transaction!
-
-  def change
+  def up
+    old_timeout = select_value("SHOW lock_timeout")
+    safety_assured { execute "SET lock_timeout = '5s';" }
     add_column :users, :wrapped_share_token, :string
-    add_index :users, :wrapped_share_token, unique: true, algorithm: :concurrently
+    safety_assured { execute "SET lock_timeout = '#{old_timeout}';" }
+  end
+
+  def down
+    remove_column :users, :wrapped_share_token
   end
 end
